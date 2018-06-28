ac5dfef927e73a7ecf6b52a252feac79c4556401
md_dev <m.damian9@hotmail.com>
2018-06-27 22:51:57 -0700

    New functions added, push form data into firebase & append new row with firebase data to page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
 $("#submit-form").on("click", function(e) {
 
     e.preventDefault();
@@ -17,5 +19,28 @@ $("#submit-form").on("click", function(e) {
     var name = $("#name-input").val().trim();
     var rating = $("#rating-input").val().trim();
     var review = $("#review-input").val().trim();
+
+    $("#name-input").val("");
+    $("#rating-input").val("");
+    $("#review-input").val("");
+
+    database.ref().push({
+        name: name,
+        rating: rating,
+        review: review
+    });
+
+});
+
+// When a new child is added to database (pushed), execute the following function:
+database.ref().on("child_added", function(snap){
+
+    var name = snap.val().name;
+    var rating = snap.val().rating;
+    var review = snap.val().review;
+
+    var newMovie = $("<tr><td>" + name + "</td><td>" + rating + "/10</td><td>" + review + "</td><tr>");
+
+    $("#movie-table-body").append(newMovie);
 
 });
